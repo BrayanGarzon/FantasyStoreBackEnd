00899179c26a7961687a849e5795136313e3cf2a
clavijo99 <bclavijogomez@gmail.com>
2025-05-11 13:25:38 -0500

    add database url

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,11 +1,16 @@
 #!/bin/sh
 
-echo "Esperando a que la base de datos esté lista..."
-until pg_isready -h db -p 5432 -U postgres
-do
-  echo "Esperando a que la base de datos se inicie..."
+DB_HOST="dpg-d0gego2dbo4c73balfs0-a.oregon-postgres.render.com"
+DB_PORT="5432"
+DB_USER="example_v761_user"
+
+echo "Esperando a que la base de datos en $DB_HOST:$DB_PORT esté lista..."
+
+until pg_isready -h "$DB_HOST" -p "$DB_PORT" -U "$DB_USER" > /dev/null 2>&1; do
+  echo "Base de datos aún no disponible. Reintentando..."
   sleep 2
 done
-echo "Base de datos lista!"
+
+echo "✅ Base de datos lista. Ejecutando comando de desarrollo..."
 
 exec /scripts/command-dev.sh
